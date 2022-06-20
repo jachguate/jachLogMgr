c7a149e9f2c3302ecd712904be809aeb4b749cb0
jachguate <jachguate@gmail.com>
2022-06-19 19:04:40 -0600

    VCLDesktop demo update

--- demos/AppType/VCLDesktop/ufrmMain.pas
+++ demos/AppType/VCLDesktop/ufrmMain.pas
@@ -32,6 +32,8 @@ uses
 
 procedure TfrmMain.Button1Click(Sender: TObject);
 begin
+  jachLog.DebugVerbosityThreshold := 3;
+  jachLog.LogLevel[jachLog.DefaultTopic] := llAll;
   Screen.Cursor := crHourGlass;
   try
     try
@@ -41,10 +43,47 @@ begin
        + 'To make the example look like a real process, we will add some entries to the log that simulates a real processing'
         );
       jachLog.LogInfo('Scanning input files...');
-      Sleep(500);
+      jachLog.LogDebug(0, 'The input filename is c:\input.txt');
+      jachLog.LogDebug(1, 'File read starts');
+      Sleep(100);
+      jachLog.LogDebug(3, '1000 records readed so far');
+      Sleep(100);
+      jachLog.LogDebug(3, '2000 records readed so far');
+      Sleep(100);
+      jachLog.LogDebug(3, '3000 records readed so far');
+      Sleep(100);
+      jachLog.LogDebug(3, '4000 records readed so far');
+      Sleep(100);
+      jachLog.LogDebug(3, '5000 records readed so far');
+      Sleep(100);
+      jachLog.LogDebug(3, '6000 records readed so far');
+      Sleep(48);
+      jachLog.LogDebug(3, '6872 records readed');
+      jachLog.LogDebug(1, 'File read end');
       jachLog.LogInfo('Consolidating information...');
-      Sleep(987);
-      jachLog.LogWarning('  this is a Warning example!');
+      jachLog.LogDebug(1, 'Data processing starts');
+      Sleep(88);
+      jachLog.LogDebug(3, '348 summary records created');
+      Sleep(88);
+      jachLog.LogDebug(3, '1342 input records processed');
+      Sleep(88);
+      jachLog.LogDebug(3, '2654 input records processed');
+      Sleep(88);
+      jachLog.LogDebug(3, '4058 input records processed');
+      Sleep(14);
+      jachLog.LogWarning('The record #4201 is malformed in the input file, ignoring it!');
+      Sleep(88);
+      jachLog.LogDebug(3, '5362 input records processed');
+      Sleep(88);
+      jachLog.LogDebug(3, '6614 input records processed');
+      Sleep(12);
+      jachLog.LogDebug(3, '6872 input records processed');
+      jachLog.LogDebug(1, 'Data processing ends');
+      jachLog.LogDebug(0, 'The output filename is c:\consolidated.txt');
+      Sleep(58);
+      jachLog.LogDebug(3, '182 summary records written so far');
+      Sleep(58);
+      jachLog.LogDebug(3, '348 summary records written so far');
       jachLog.LogInfo('Connecting to database...');
       Sleep(312);
       jachLog.LogAlert('  this is an Alert example!');
